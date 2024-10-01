1c9c4697b149366392461b95d38763d0c5b71cbd
Felipe Marques <fm043445@gmail.com>
2024-10-01 10:12:34 -0300

    Adicionando as vantagens de viajar conosco

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -2,15 +2,34 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Image1 from '../../assets/rio-img.jpg';
 import Image2 from '../../assets/deserto-img.jpg';
-import Image3 from '../../assets/van-img.jpg'
-import './sobre.css'; 
+import Image3 from '../../assets/van-img.jpg';
+import './sobre.css';
 
 const Sobre = () => {
+    
+    const vantagens = [
+        {
+            title: 'Pagar no PIX',
+            text: 'Facilidade e segurança no pagamento. Faça suas reservas de forma rápida e prática usando PIX.',
+            variant: 'Primary', 
+        },
+        {
+            title: 'Seguro Viagem',
+            text: 'Viaje com tranquilidade! Oferecemos opções de seguro viagem para que você tenha a proteção que precisa.',
+            variant: 'Secondary',
+        },
+        {
+            title: 'Atendimento 24/7',
+            text: 'Nosso suporte está disponível a qualquer hora. Estamos aqui para ajudar você em sua viagem, a qualquer momento.',
+            variant: 'Info',
+        },
+    ];
+
     return (
         <>
             <section className='container'>
                 <div className="row justify-content-center">
-                    <div className="col-md-4"> {/* Coluna responsiva para os cards */}
+                    <div className="col-md-4">
                         <Card style={{ width: '100%' }}>
                             <Card.Img variant="top" src={Image1} />
                             <Card.Body>
@@ -48,6 +67,27 @@ const Sobre = () => {
                             </Card.Body>
                         </Card>
                     </div>
+                </div>
+
+                <div className="row justify-content-center mt-4"> 
+                    {vantagens.map((vantagem, index) => (
+                        <div className="col-md-4" key={index}>
+                            <Card
+                                bg={vantagem.variant.toLowerCase()}
+                                text={vantagem.variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                                style={{ width: '100%' }} 
+                                className="mb-2"
+                            >
+                                <Card.Header>Vantagens</Card.Header>
+                                <Card.Body>
+                                    <Card.Title>{vantagem.title}</Card.Title>
+                                    <Card.Text>
+                                        {vantagem.text}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ))}
                 </div>
             </section>
         </>
